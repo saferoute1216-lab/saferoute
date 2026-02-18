f5f3bc9ca4991a7c6bf3f0465a95b5ad10bcbf19
saferoute1216-lab <saferoute1216@gmail.com>
2026-02-18 19:23:44 +0800

    Update ProfileController.php

--- controllers/ProfileController.php
+++ controllers/ProfileController.php
@@ -3,7 +3,12 @@ session_start();
 
 require_once __DIR__ . '/../models/FamilyModel.php';
 
-/* logout handler */
+if (!isset($_SESSION['user_id'])) {
+    header("Location: ../index.php");
+    exit;
+}
+
+/* logout */
 if (isset($_GET['action']) && $_GET['action'] === 'logout') {
     session_unset();
     session_destroy();
@@ -11,21 +16,18 @@ if (isset($_GET['action']) && $_GET['action'] === 'logout') {
     exit;
 }
 
-/* must be logged in */
-if (!isset($_SESSION['user_id'])) {
-    header("Location: ../index.php");
-    exit;
-}
-
 $user_id = $_SESSION['user_id'];
 
-/* ask model */
-$family_id = FamilyModel::userHasFamily($user_id);
-
+/* ALWAYS define defaults */
+$family_id = null;
 $family_members = [];
+
+/* ask model */
+$family_id = FamilyModel::getUserFamilyId($user_id);
+
 if ($family_id) {
     $family_members = FamilyModel::getFamilyMembers($family_id);
 }
 
-/* pass data to view */
-require __DIR__ . '/../views/profile.php';
+header("Location: ../views/profile.php");
+exit;
